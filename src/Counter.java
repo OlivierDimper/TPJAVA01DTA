6544dc4096a18c52ca7272e7ab5ad84074ac48a9
Olivier Dimper <olivier.dimper@gmail.com>
2018-04-11 11:18:45 +0200

    initialisation des attributs et des méthodes de base de la classe counter

--- src/Counter.java
+++ src/Counter.java
@@ -1,4 +1,22 @@
 
 public class Counter {
-
+	private int increment = 1;
+	private int valeur = 0;
+	
+	public int getValue() {
+		return this.valeur;
+	}
+	
+	public void inc() {
+		this.valeur++;
+	}
+	
+	public String toString() {
+		String chaine = "valeur : " + this.valeur + " (increment " + this.increment + ")";
+		return chaine;
+	}
+	
+	public boolean equals(Counter counter) {
+		return this.valeur == counter.valeur;
+	}
 }
